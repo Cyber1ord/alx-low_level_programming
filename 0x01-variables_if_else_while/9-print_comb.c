32fc3ddbb4977825dca64a562da1285ae4ff262b
Cyber1ord <rooda43@gmail.com>
2023-02-17 13:45:56 -0800

    Task 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,21 +10,15 @@
  */
 int main(void)
 {
-	int i, j;
-	
+	int i;
+
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 10; j++)
+		putchar(i + '0');
+		if (i < 9)
 		{
-			if (i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8 || j != 9)
-				{
-					putchar(',' && ' ');
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
