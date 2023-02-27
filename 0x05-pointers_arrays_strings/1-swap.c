2b423d3c79d5f64e540a5361f4c7c91140218b4f
Cyber1ord <rooda43@gmail.com>
2023-02-27 08:50:40 -0800

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,6 +4,8 @@
 /**
 * swap_int - function that takes a pointer to an int as
 * parameter and updates the value it points to to 98.
+* @a: First integer
+* @b: Second integer
 * Returns: Always 0
 */
 void swap_int(int *a, int *b)
