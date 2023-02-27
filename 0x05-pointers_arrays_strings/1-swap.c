3a41e8b5f53ffb149ffc1a8c8aa13bf4676c7159
Cyber1ord <rooda43@gmail.com>
2023-02-27 08:55:06 -0800

    Task 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
-* swap_int - function that takes a pointer to an int as
-* parameter and updates the value it points to to 98.
-* @a: First integer
-* @b: Second integer
-* Returns: Always 0
-*/
+ * swap_int - function that takes a pointer to an int as
+ * parameter and updates the value it points to to 98.
+ * @a: First integer
+ * @b: Second integer
+ * Returns: Always 0
+ */
 void swap_int(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
