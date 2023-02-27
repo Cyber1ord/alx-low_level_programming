fb75fb59045d7a0d0e1f36c8abb65f977caa0edd
Cyber1ord <rooda43@gmail.com>
2023-02-27 08:10:24 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+include <stdio.h>
+
+/**
+* main - check code
+* Returns: Always 0
+*/
+int main(void)
+{
+    int n;
+
+    *p = %n;
+    p = 98;
+    printf("n=%d\n", n);
+    return (0);
+}
