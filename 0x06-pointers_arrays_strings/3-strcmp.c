c07d0e9bb294d54c2a5627c87379638d8110df4f
Cyber1ord <rooda43@gmail.com>
2023-03-02 15:34:04 -0800

    TAsk 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,54 +1,23 @@
-#include <stdio.h>
 #include "main.h"
+#include "holberton.h"
 
 /**
- * infinite_add - function that adds two numbers.
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * _strcmp - function that compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 if s1 and s2 are equal, negative number if s1 is less than s2,
+ *         positive number if s1 is greater than s2
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int _strcmp(char *s1, char *s2)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int i;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+
+	return (s1[i] - s2[i]);
 }
