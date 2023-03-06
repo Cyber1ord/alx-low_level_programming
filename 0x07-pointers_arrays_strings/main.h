4126c33d19ce809dc75b49dfe3bbf05d59df0d0f
Cyber1ord <rooda43@gmail.com>
2023-03-06 09:01:56 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ *_memset - fills memory with a constant byte
+ * @s: character 1
+ * @b: character 2
+ * @n: unsigned integer
+ * Return: 1 or 0
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+#endif
