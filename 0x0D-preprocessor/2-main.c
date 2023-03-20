16e6b2ff2073c8e2df3c96655541b5944024bf30
Cyber1ord <rooda43@gmail.com>
2023-03-20 14:55:27 -0700

    function-like macro SUM(x, y) that computes the sum of the numbers x and y

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,11 @@
+#include <stdlib.h>
 #include <stdio.h>
-
-int main(void) {
-  printf("Compiled from file: %s\n", __FILE__);
-  return 0;
+/**
+ * main - main file returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
-
