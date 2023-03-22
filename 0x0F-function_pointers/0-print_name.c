61da1753f66f3337f0241169a85a7402372a48db
Cyber1ord <rooda43@gmail.com>
2023-03-22 16:38:22 -0700

    function that executes a function given as a parameter on each element of an array.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
+#include "function_pointers.h"
+
 /**
  * print_name - Prints a name
+ * @name: The name to prints
+ * @f: Pointer to fuction
  *
- * @name: Pointer to the name to print
- * @f: Pointer to the function to use to print the name
- *
- * Return: void
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		f(name);
+	f(name);
 }
 
