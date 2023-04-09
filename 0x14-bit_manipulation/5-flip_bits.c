f84c2550167a0837d117d74d5bf878d12f1c18dd
Cyber1ord <rooda43@gmail.com>
2023-04-09 15:19:23 -0700

    C - Bit manipulation gcp .

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from one number to another
+ * @n: number one.
+ * @m: number two.
+ *
+ * Return: number of bits.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flip_no, i;
+	flip_no = n ^ m;
+	i = 0;
+
+	while (flip_no)
+	{
+		if (flip_no & 1)
+			count++;
+		flip_no >>= 1;
+	}
+
+	return count;
+}
