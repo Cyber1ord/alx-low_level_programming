6df10cdfa7ef5c93b79e1b134562d3c7994d348f
Cyber1ord <rooda43@gmail.com>
2023-05-22 13:02:52 -0700

    dynamic library libdynamic.so containing all the functions

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,79 @@
+int _putchar(char c) {
+    /* Code here */
+}
+
+int _islower(int c) {
+    /* Code here */
+}
+
+int _isalpha(int c) {
+    /* Code here */
+}
+
+int _abs(int n) {
+    /* Code here */
+}
+
+int _isupper(int c) {
+    /* Code here */
+}
+
+int _isdigit(int c) {
+    /* Code here */
+}
+
+int _strlen(char *s) {
+    /* Code here */
+}
+
+void _puts(char *s) {
+    /* Code here */
+}
+
+char *_strcpy(char *dest, char *src) {
+    /* Code here */
+}
+
+int _atoi(char *s) {
+    /* Code here */
+}
+
+char *_strcat(char *dest, char *src) {
+    /* Code here */
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    /* Code here */
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    /* Code here */
+}
+
+int _strcmp(char *s1, char *s2) {
+    /* Code here */
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    /* Code here */
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    /* Code here */
+}
+
+char *_strchr(char *s, char c) {
+    /* Code here */
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    /* Code here */
+}
+
+char *_strpbrk(char *s, char *accept) {
+    /* Code here */
+}
+
+char *_strstr(char *haystack, char *needle) {
+    /* Code here */
+}
